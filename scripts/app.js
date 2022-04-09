019448df13ce435f301b691daf79b60a32bf60eb
shirtol <shirzahavi7@gmail.com>
2022-04-09 14:52:57 +0300

    Added script for changing the big image when hover

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+$("#img-preview-1").hover(() => {
+    console.log("hiiiiiii");
+    $("#big-img").attr("src", "../assets/images/item1-big1.jpg");
+});
+$("#img-preview-2").hover(() => {
+    console.log("hiiiiiii");
+    $("#big-img").attr("src", "../assets/images/item1-big2.jpg");
+});
+$("#img-preview-3").hover(() => {
+    console.log("hiiiiiii");
+    $("#big-img").attr("src", "../assets/images/item1-big3.jpg");
+});
+$("#img-preview-4").hover(() => {
+    console.log("hiiiiiii");
+    $("#big-img").attr("src", "../assets/images/item1-big4.jpg");
+});
+$("#img-preview-5").hover(() => {
+    console.log("hiiiiiii");
+    $("#big-img").attr("src", "../assets/images/item1-big5.jpg");
+});
+$("#img-preview-6").hover(() => {
+    console.log("hiiiiiii");
+    $("#big-img").attr("src", "../assets/images/item1-big6.jpg");
+});
+$("#img-preview-7").hover(() => {
+    console.log("hiiiiiii");
+    $("#big-img").attr("src", "../assets/images/item1-big7.jpg");
+});
