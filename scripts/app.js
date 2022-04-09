aaf48ea0fb5a1a15725430ca208c32b0c4ecdd1d
shirtol <shirzahavi7@gmail.com>
2022-04-09 15:24:06 +0300

    Start working on clickable items

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// Change big image when hover on the preview images:
+
 $("#img-preview-1").hover(() => {
     $("#big-img").attr("src", "../assets/images/item1-big1.jpg");
 });
@@ -18,4 +20,15 @@ $("#img-preview-6").hover(() => {
 });
 $("#img-preview-7").hover(() => {
     $("#big-img").attr("src", "../assets/images/item1-big7.jpg");
+});
+
+/* 
+!TODO: change big image when hover on items 
+! change preview when click on items (just 1 and 7 preview images need to change.)
+! 
+*/
+
+// Change preview images when click on item:
+$(".item-chooser2__container").click(() => {
+    $("#preview-1").attr("src", "");
 });
